c154c5a0fbeab28d38c206c23db75f1b8d40acf9
Rabea <rabea3.kadd@hotmail.com>
2026-05-11 22:36:09 +0300

    fix(terraform): fix dev and staging backends

--- terraform/dev/backend.tf
+++ terraform/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "ecom-tf-backend-s3-bucket"
+    bucket       = "ecom-tf-backend-dev-s3-bucket"
     key          = "ecom-eks/terraform"
     region       = "us-east-1"
     use_lockfile = true
