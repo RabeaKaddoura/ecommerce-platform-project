1e13ce610c0333c3a3391cc54540925df4473998
Rabea <rabea3.kadd@hotmail.com>
2026-05-14 04:00:06 +0300

    fix(backend): fix db port parsing issue for all services

--- backend/auth-service/db/session.py
+++ backend/auth-service/db/session.py
@@ -4,11 +4,11 @@ import os
 load_dotenv()
 
 def init_db(app):
-    db_url = f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    db_url = f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{int(os.getenv('DB_PORT'))}/{os.getenv('DB_NAME')}"
     register_tortoise(
         app,
         db_url=db_url,
         modules={"models": ["models.user_model"]},
         generate_schemas=True,
         add_exception_handlers=True,
-    )
+    ) 
--- backend/product-service/db/session.py
+++ backend/product-service/db/session.py
@@ -4,7 +4,7 @@ import os
 load_dotenv()
 
 def init_db(app):
-    db_url = f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{os.getenv('DB_PORT')}/{os.getenv('DB_NAME')}"
+    db_url = f"postgres://{os.getenv('DB_USER')}:{os.getenv('DB_PASSWORD')}@{os.getenv('DB_HOST')}:{int(os.getenv('DB_PORT'))}/{os.getenv('DB_NAME')}"
     register_tortoise(
         app,
         db_url=db_url,
