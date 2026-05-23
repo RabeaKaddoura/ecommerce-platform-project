22ad3ece1eb914d75b2f91a8b49275db0c5a49aa
Rabea <rabea3.kadd@hotmail.com>
2026-05-23 09:18:21 +0300

    fix(frontend): fix unreadable environment variables

--- frontend/docker-entrypoint.sh
+++ frontend/docker-entrypoint.sh
@@ -3,8 +3,8 @@
 #Generate runtime config from environment variables
 cat <<EOF > /usr/share/nginx/html/config.js
 window.__CONFIG__ = {
-  CLOUDFRONT_URL: "${CLOUDFRONT_URL}",
-  STRIPE_PUBLISHABLE_KEY: "${STRIPE_PUBLISHABLE_KEY}"
+  CLOUDFRONT_URL: "${CLOUDFRONT_URL:-$VITE_CLOUDFRONT_URL}",
+  STRIPE_PUBLISHABLE_KEY: "${STRIPE_PUBLISHABLE_KEY:-$VITE_STRIPE_PUBLISHABLE_KEY}"
 };
 EOF
 
