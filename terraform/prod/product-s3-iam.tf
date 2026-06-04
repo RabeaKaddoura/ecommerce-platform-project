599d8f00a5a8d3af78644b02465af01f54ccf684
Rabea <rabea3.kadd@hotmail.com>
2026-06-04 17:21:54 +0300

    refactor(terraform): change hardcoded role name in product-s3-iam file

--- terraform/prod/product-s3-iam.tf
+++ terraform/prod/product-s3-iam.tf
@@ -12,5 +12,5 @@ resource "aws_iam_policy" "product_s3" {
 
 resource "aws_iam_role_policy_attachment" "product_s3" {
   policy_arn = aws_iam_policy.product_s3.arn
-  role       = "ecom-secrets-csi-role" #backend-sa role shared by all services
+  role       = "${var.prefix}-secrets-csi-role" #backend-sa role shared by all services
 }
